0e19e019998d7756871f5b2548f7857d1d149fb8
Oleksii Kostenikov <akotenikov@gmail.com>
2025-09-09 21:58:14 +0300

    refactor permission.py again, fix flake8

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -3,6 +3,9 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if (request.method in SAFE_METHODS and request.user.is_authenticated) or request.user.is_staff:
+        if (
+                request.method in SAFE_METHODS
+                and request.user.is_authenticated
+        ) or request.user.is_staff:
             return True
         return False
